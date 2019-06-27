706ec8b77adcb58fcd41b975fe91c20b34595e99
Edward Zempel <by8477ks@metrostate.edu>
2019-06-27 08:44:46 -0500

    added pytest

---
+++ pytest.py
@@ -0,0 +1,12 @@
+# import antigravity
+# multiple cursors with alt+ click, escape to exit
+print("Hello world")
+
+words = ["who", "are", "you"]
+words.insert(-1,'today')
+
+for word in words:
+    print(word)
+
+for i in range(1, 5):
+    print(i)
