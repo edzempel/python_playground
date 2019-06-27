71093e4050d715a80afcdea08319283eb05f32d4
Edward Zempel <by8477ks@metrostate.edu>
2019-06-27 15:20:17 -0500

    added test depth for inc_dec

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -2,8 +2,12 @@ import inc_dec  # code to test
 
 
 def test_increment():
-    assert inc_dec.increment(3) == 4
+    assert inc_dec.increment(1) == 2
+    assert inc_dec.increment(-1) == 0
+    assert inc_dec.increment(0) == 1
 
 
 def test_decrement():
-    assert inc_dec.decrement(3) == 4
+    assert inc_dec.decrement(1) == 0
+    assert inc_dec.decrement(-1) == -2
+    assert inc_dec.decrement(0) == -1
