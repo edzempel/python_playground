880278a0d28be7ac4533bf9616369b479b7c7ca7
Edward Zempel <by8477ks@metrostate.edu>
2019-06-27 10:32:43 -0500

    basic error command line error handling

--- regex/schedule_formatter.py
+++ regex/schedule_formatter.py
@@ -4,8 +4,12 @@ import os
 
 def main():
     print_header()
-    print_command_line_args()
-    print(get_data_file())
+    # print_command_line_args()
+    # print(get_data_file())
+    if check_command_line_args():
+        filename = get_data_file()
+        file = load_file(filename)
+        print(type(file))
 
 
 def print_header():
@@ -20,9 +24,30 @@ def print_command_line_args():
     print("Available args:", sys.argv)
 
 
+def check_command_line_args():
+    if len(sys.argv) <= 1:
+        print(
+            "Missing argument! Specify target file name after script. It must be in same directory as script."
+        )
+        print("Example: schedule_formatter.py sample.txt")
+        return False
+    elif sys.argv[1] == "--help":
+        print("This script formats copied schedule text to paste into spreadsheet.")
+        print("Provide the name of the  target txt file after the script")
+        print("Example: schedule_formatter.py sample.txt")
+        return False
+    elif sys.argv[1]:
+        return True
+
+
 def get_data_file():
     base_folder = os.path.dirname(__file__)
     return os.path.join(base_folder, sys.argv[1])
+
+
+def load_file(filename):
+    with open(filename, "r", encoding="utf-8") as fin:
+        return fin
 
 
 def regex_swap_last_two_columns():
