95b7ecceacce530a0c1db384be2b09718eb4bbd0
Edward Zempel <by8477ks@metrostate.edu>
2019-06-27 10:19:48 -0500

    access filename from command arg

--- regex/schedule_formatter.py
+++ regex/schedule_formatter.py
@@ -1,9 +1,11 @@
 import sys
+import os
 
 
 def main():
     print_header()
     print_command_line_args()
+    print(get_data_file())
 
 
 def print_header():
@@ -16,6 +18,11 @@ def print_header():
 def print_command_line_args():
     print("Total args:", len(sys.argv))
     print("Available args:", sys.argv)
+
+
+def get_data_file():
+    base_folder = os.path.dirname(__file__)
+    return os.path.join(base_folder, sys.argv[1])
 
 
 def regex_swap_last_two_columns():
